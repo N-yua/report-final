f216436b7f8012c74c4c36304ecd0a4ae41eebad
naka yua <e235743@ie.u-ryukyu.ac.jp[>
2024-01-23 02:20:45 +0900

    last

--- src/jp/ac/uryukyu/ie/e235743/Main.java
+++ src/jp/ac/uryukyu/ie/e235743/Main.java
@@ -13,6 +13,7 @@ public class Main {
         Monster dog = new Monster("いぬ", 5,0);
         Player player = new Player("ゆあ", 20,"ジャーキー");
         List<String> list = new ArrayList<String>();
+        
         /*モンスターが現れた */
         dog.appearance();
         /*ご飯をあげる、ゲットする、逃げるから選択するメソッド<br>
