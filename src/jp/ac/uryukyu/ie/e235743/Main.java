575c2b8bcd8da23de0217b61c55741f50dd79b61
naka yua <e235743@ie.u-ryukyu.ac.jp[>
2024-01-22 19:48:42 +0900

    four

--- src/jp/ac/uryukyu/ie/e235743/Main.java
+++ src/jp/ac/uryukyu/ie/e235743/Main.java
@@ -10,6 +10,53 @@ public class Main {
     /*インスタンス */
         Monster dog = new Monster("いぬ", 5,0);
         Player player = new Player("ゆあ", 20,"ジャーキー");
-
+        /*モンスターが現れた */
+        dog.appearance();
+        /*ご飯をあげる、ゲットする、逃げるから選択するメソッド<br>
+         * プレイヤーのHPが０を下回るまでループする<br>
+         */
+        try (java.util.Scanner sc = new java.util.Scanner(System.in)) {
+                while (player.getHp() > 0) {
+                    /*モンスターのステータスを表示する<br>
+                     * ご飯を与え続けると2ずつカウントされる
+                     */
+                    dog.sttatus();
+                    final String[] Types = {"1.ご飯をあげる","2.ゲットする","3.逃げる"};
+                    /*配列の中身を全て表示する */
+                    for(int i = 0; i<3; i++){
+                        System.out.println(Types[i]);
+                    }
+                    /*プレイヤーの入力を受け取る */
+                    int choice = sc.nextInt();
+                    /*選択肢１の場合、モンスターにご飯を与える<br>
+                     * ご飯を与えるとモンスターは好感度が２ずつ上昇する
+                     */
+                    if (choice == 1) {
+                        System.out.println(player.getFood() +"をあげた！");
+                        dog.eat();
+                    /* 選択肢２の場合、モンスターをゲットすることができる<br>
+                     * ただし、好感度が10以下の場合攻撃される<br>
+                     * ゲットできた時は強制終了する
+                    */
+                    } else if (choice == 2) {
+                        System.out.println("ゲットする！");
+                        if (dog.getGet() <= 10) {
+                            System.out.println("捕まえられなかった！");
+                            dog.attack(dog,player);
+                        }else{
+                            dog.lovepoint();
+                            System.out.println(dog.name+"を仲間にした！");
+                            break;
+                        }
+                    /*選択肢３の場合、ループ処理を強制終了する */
+                    } else if (choice == 3) {
+                        System.out.println("逃げ出した！");
+                        break;
+                    /*その他の入力があった場合、もう一度初めからやり直す */
+                    } else {
+                        System.out.println("無効な選択です。もう一度選んでください。");
+                    }
+                }
+            }
     }
 }
